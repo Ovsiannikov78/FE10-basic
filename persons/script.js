5815f36b28c93c36586bd9e55a29495330badfe0
Ovsiannikov78 <seri_tanya@mail.ru>
2020-05-15 13:05:48 +0200

    HW Persons

--- persons/script.js
+++ persons/script.js
@@ -22,17 +22,13 @@ const sortedPersons = persons.sort(function (a, b) {
         return 0;
 });
 
-<<<<<<< HEAD
 const sortedPersonsByAge = persons.sort(function (a, b) { return b.age - a.age });
 
-=======
->>>>>>> 30bc322e96a019e4e1d709ced9ae4d2b3b510366
 let innerHtmlTableBody = '';
 for (let i = 0; i < personsNumber; i++) {
     innerHtmlTableBody += '<tr><td>' + sortedPersons[i].name + '</td><td>' + sortedPersons[i].age + '</td></tr>';
     console.log('Person ' + (i + 1) + ': name = ' + sortedPersons[i].name + ' age = ' + sortedPersons[i].age);
 }
-<<<<<<< HEAD
 let innerHTMLTableTfoot = '';
     innerHTMLTableTfoot = '<div><p>' + 'Avarage age ' + ' ---------- ' + averageAge + '</p></div>';
 
@@ -41,11 +37,3 @@ document.getElementsByTagName('p')[0].innerHTML = innerHTMLTableTfoot ;
 
 console.log('Average age of person:' + averageAge);
 
-=======
-
-document.getElementsByTagName('tbody')[0].innerHTML = innerHtmlTableBody;
-
-console.log('Average age of person:' + averageAge);
-
-console.log('Hello, World');
->>>>>>> 30bc322e96a019e4e1d709ced9ae4d2b3b510366
