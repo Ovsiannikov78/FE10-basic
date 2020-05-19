042e0618e5f0c9725646f9048391f30955b36b4e
Ovsiannikov78 <seri_tanya@mail.ru>
2020-05-19 21:00:27 +0200

    Added fixes

--- persons/script.js
+++ persons/script.js
@@ -4,34 +4,59 @@ function showUsersTable(persons = []) {
         return a.age - b.age;
     });
 
+    let minAge = persons[0].age;
+    let maxAge = persons[0].age;
     let averageAge = 0;
     let innerHtmlTableBody = '';
-        for (let i = 0; i < persons.length; i++) {
-            innerHtmlTableBody += '<tr><td>' + sortedPersons[i].name + '</td><td>' + sortedPersons[i].age + '</td></tr>';
-            console.log('Person ' + (i + 1) + ': name = ' + sortedPersons[i].name + ' age = ' + sortedPersons[i].age);
-            averageAge += + sortedPersons[i].age;
+    for (let i = 0; i < persons.length; i++) {
+        innerHtmlTableBody += '<tr><td>' + sortedPersons[i].firstName + '</td><td>' + sortedPersons[i].lastName + '</td><td>' + sortedPersons[i].age + '</td></tr>';
+        console.log('Person ' + (i + 1) + ': firstName = ' + sortedPersons[i].firstName + ' firstName = ' + sortedPersons[i].firstName + ' age = ' + sortedPersons[i].age);
+        averageAge += + sortedPersons[i].age;
+        if (persons[i].age > maxAge) {
+            maxAge = persons[i].age;
         }
+        if (persons[i].age < minAge) {
+            minAge = persons[i].age;
+    }
+}
     averageAge /= persons.length;
 
     document.getElementsByTagName('tbody')[0].innerHTML = innerHtmlTableBody;
 
     console.log('Average age of person:' + averageAge);
     document.querySelector('tfoot div.alert').innerHTML = 'Average age: ' + averageAge;
+
+    console.log('Max age of person:' + maxAge);
+    document.getElementById("max").innerHTML = 'Max age: ' + maxAge;
+
+    console.log('Min age of person:' + minAge);
+    document.getElementById("min").innerHTML = 'Min age: ' + minAge; 
+
 }
 
 let persons = [];
-let averageAge = 0;
 const personsNumber = prompt('Please enter the number of persons:');
 console.log(personsNumber);
 
 
 for (let i = 0; i < personsNumber; i++) {
-    setTimeout(function() {
+    setTimeout(function () {
         const person = {};
-        person.name = prompt('Please enter name of the ' + (i + 1) + ' person');
+        person.firstName = prompt('Please enter First Name of the ' + (i + 1) + ' person');
+        person.lastName = prompt('Please enter Last Name of the ' + (i + 1) + ' person');
         person.age = prompt('Please enter age of the ' + (i + 1) + 'person');
         persons[i] = person;
 
         showUsersTable(persons);
     }, 500)
-}    
+}
+
+
+
+
+
+
+
+
+
+ 
