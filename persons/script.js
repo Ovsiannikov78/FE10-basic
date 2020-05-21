53f50e45e3c83c18ec05bcaa83f38f9de9ef215f
Ovsiannikov78 <seri_tanya@mail.ru>
2020-05-21 10:14:33 +0200

    Added new field city in form and in table

--- persons/script.js
+++ persons/script.js
@@ -9,7 +9,7 @@ function showUsersTable(persons = []) {
     let maxAge = persons[0].age;
     let innerHtmlTableBody = '';
     for (let i = 0; i < persons.length; i++) {
-        innerHtmlTableBody += '<tr><td>' + sortedPersons[i].name + '</td><td>' + sortedPersons[i].age + '</td></tr>';
+        innerHtmlTableBody += '<tr><td>' + sortedPersons[i].name + '</td><td>' + sortedPersons[i].age + '</td><td>' + sortedPersons[i].city + '</td></tr>';
         averageAge += + sortedPersons[i].age;
         if (persons[i].age > maxAge)
             maxAge = persons[i].age;
@@ -19,34 +19,11 @@ function showUsersTable(persons = []) {
     averageAge /= persons.length;
 
     document.getElementsByTagName('tbody')[0].innerHTML = innerHtmlTableBody;
-    document.querySelector('tfoot div.alert').innerHTML = 'Average age: ' + averageAge + ' Min age: ' + minAge + ' Max age: ' + maxAge;
+    document.querySelector('tfoot div.alert').innerHTML = 'Average age: ' + averageAge + ' --------  Min age: ' + minAge + ' ---------  Max age: ' + maxAge;
 }
 
 let persons = [];
 
-<<<<<<< HEAD
-for (let i = 0; i < personsNumber; i++) {
-    setTimeout(function () {
-        const person = {};
-        person.firstName = prompt('Please enter First Name of the ' + (i + 1) + ' person');
-        person.lastName = prompt('Please enter Last Name of the ' + (i + 1) + ' person');
-        person.age = prompt('Please enter age of the ' + (i + 1) + 'person');
-        persons[i] = person;
-
-        showUsersTable(persons);
-    }, 500)
-}
-
-
-
-
-
-
-
-
-
- 
-=======
 const form$ = document.getElementsByTagName('form')[0];
 form$.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -54,9 +31,9 @@ form$.addEventListener('submit', function(event) {
     let person = {};
     person.name = document.querySelector('input[name=name]').value;
     person.age = document.querySelector('input[name=age]').value;
+    person.city = document.querySelector('input[name=city]').value;
 
     persons.push(person);
 
     showUsersTable(persons);
 })
->>>>>>> 77547d988f331762f2faf1023fbf6b017ea018e1
