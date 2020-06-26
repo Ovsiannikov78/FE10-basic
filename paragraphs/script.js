453c23834bc3023a675105547dc23624a08636b6
Ovsiannikov78 <seri_tanya@mail.ru>
2020-06-26 17:22:34 +0200

    Paragraphs project using closures

--- paragraphs/script.js
+++ paragraphs/script.js
@@ -1,11 +1,35 @@
 const colors = ['white', 'red', 'green', 'blue'];
 //                 0       1       2        3  
 
-let currentColor = 0;
+function eventListener(element) {
+    let currentColor = 0;
+    return function () {
+        currentColor++;
+        return element.style['background-color'] = colors[currentColor % colors.length];
+    }
+}
+document.querySelectorAll('p').forEach(function (element) {
+    element.addEventListener('click', eventListener(element));
+})
+
+
+
+/* let currentColor = 0;
 document.querySelectorAll('p').forEach(function(element) {
     element.addEventListener('click', function() {
         currentColor++;
         this.style['background-color'] = colors[currentColor % colors.length];
+
     });
 })
+
+ */
+
+
+
+
+
+
+
+
 
