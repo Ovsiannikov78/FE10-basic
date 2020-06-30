7ed857470a5f8d96d569bdd0af3e6c31787fdff0
Ovsiannikov78 <seri_tanya@mail.ru>
2020-06-30 19:09:24 +0200

    The number are still not unique

--- Print_random_number_by_pushing_a_button/script.js
+++ Print_random_number_by_pushing_a_button/script.js
@@ -1,30 +1,12 @@
-/* let nodes = document.getElementsByTagName('button');
+let nodes = document.getElementsByTagName('button');
 for (let i = 0; i < nodes.length; i++) {
    nodes[i].addEventListener('click', function Random(x) {
       const max = 10;
       return function () {
-         console.log('Element # ' + x + ' with random number - ' + Math.round(Math.random() * max));   // Работает. Но элементы повторяются.
-      }
-   }(i));
-} */
-
-let nodes = document.getElementsByTagName('button');
-for (let i = 0; i < nodes.length; i++) {
-   nodes[i].addEventListener('click', function Random(x) {
-      const max = 20;
-      return function () {
-         const uniqe = new Set();
-         while (uniqe.size !== max) {
-            uniqe.add(Math.floor(Math.random() * max) + 1);
-         }
-         console.log('Element # ' + x + ' with random number - ' + [...uniqe]);   /* Работает. Элементы не повторяются, но выводит все элементы коллекции.
-                                                                                        Как обратиться к каждому эл. коллекции ?  */
-      }
-   }(i));
+         const unique = new Set();
+         unique.add(Math.floor(Math.random() * max) + 1);
+         let num = unique.values();
+         console.log('Element # ' + x + ' with random number - ' + (num.next().value));  
+      }                                                                                  
+   }(i))
 }
-
-
-
-
-
-
